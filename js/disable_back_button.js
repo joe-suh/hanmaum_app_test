d92f8c6e3ba86086bbc8308a627d0a3880e0f0db
joe suh <joe.suh7@gmail.com>
2015-01-22 14:55:04 +0100

    -disabling back button -app icon

---
+++ js/disable_back_button.js
@@ -0,0 +1,20 @@
+/*
+ * Controlling sub menu
+ *
+ * Uses the built In easIng capabilities added In jQuery 1.1
+ * to offer multiple easIng options
+ *
+ * Copyright (c) 2007 George Smith
+ * Licensed under the MIT License:
+ *   http://www.opensource.org/licenses/mit-license.php
+ */
+
+
+//Device ready function
+document.addEventListener('deviceready', function() {
+    document.addEventListener("backbutton", goBack, false);
+}, false);
+
+//Function for back button function
+function goBack(){
+}
