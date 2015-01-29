00002e0bff883d76cff9d081ec08b74f65bba5a3
joe suh <joe.suh7@gmail.com>
2015-01-29 01:14:53 +0100

    updated top menu

--- js/submenu.js
+++ js/submenu.js
@@ -8,15 +8,39 @@
  * Licensed under the MIT License:
  *   http://www.opensource.org/licenses/mit-license.php
  */
-
+var isClickHandled=false;
 
 function toggleTopMenu() {
-	var menuLinks = document.getElementById('w3-menu-links');
+	var menuLinks = document.getElementById('sub-menu-links');
 	if (menuLinks.style.display != "none") {
 		menuLinks.style.display = "none";
+		isTopMenuShown=false;
 	} else {
 		menuLinks.style.display = "block";
+		isTopMenuShown=true;
 	}
+	
+	isClickHandled = true;
+	return false;
+}
+
+function goHome() {
+	isClickHandled = true;
+	location.href='index.html';
+	return false;
+}
+
+
+function handleOnClick() {
+	if ( isClickHandled )
+	{
+		isClickHandled = false;
+		return false;
+	}
+	var menuLinks = document.getElementById('sub-menu-links');
+	if (menuLinks.style.display != "none") {
+		menuLinks.style.display = "none";
+	} 
 	
 	return false;
 }
